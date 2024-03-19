fb71f9ae95a3b42d08220fa721eec35d781c7c7f
sarabluekens <sara.bluekens@skynet.be>
2024-03-19 21:15:20 +0100

    hotfix: edited cv

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
           Digital developer
         </h1>
         <Link
-          href="/cvUpdated.pdf"
+          href="/cv_updated.pdf"
           download
           className="hidden self-end rounded-lg border-b-4 border-l-2 border-r-4 border-t-2 border-solid border-blue px-4 py-2 md:inline"
         >
