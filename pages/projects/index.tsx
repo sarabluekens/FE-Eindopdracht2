309debaa668e18ca0a2b4d1dd9ceec5d2fedce12
sarabluekens <sara.bluekens@skynet.be>
2024-04-07 23:34:46 +0200

    update: new build

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -8,7 +8,7 @@ const Projects = ({ data }: { data: any }) => {
       <Navbar />
       <article id="projects" className="px-20 py-24">
         <PromoHeader title="My projects" />
-        <section className="grid-rows-auto m-16 grid grid-cols-2 gap-2">
+        <section className="grid-rows-auto m-16 grid w-2/3 grid-cols-2 gap-2">
           <h2 className="hidden">Projects</h2>
           {data.map(
             (project: any) =>
